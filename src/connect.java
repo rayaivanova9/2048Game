b2c98e811b8e352dc34493e22fe1203af3b05d66
rayaivanova9 <raya.m.ivanova@gmail.com>
2025-10-21 17:02:36 +0200

    Connection class working properly.

---
+++ src/connect.java
@@ -0,0 +1,19 @@
+import java.sql.*;
+
+
+public class connect {
+
+    public connect(String query) {
+        //String query = "INSERT INTO high_scores (username, score) VALUES ('thebest', 4218688)";
+
+        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:C:/Users/20252711/IdeaProjects/2048Game/identifier.sqlite");
+             PreparedStatement pstmt = connection.prepareStatement(query)) {
+
+            System.out.println(pstmt.executeUpdate());
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("SQL Error.");
+        }
+    }
+}
